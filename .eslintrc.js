ec633dfc3f08e408bfe4e50df3b81107e6b35a03
Luis Fernando Colorado <108040168+LuisFerColorado@users.noreply.github.com>
2023-12-30 00:22:40 -0500

    chore(init): update settings for TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {},
 };
