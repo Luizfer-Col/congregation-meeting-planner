dfb1a2e215c2eeeda81b87980ca5e1d2830ac6b8
Luis Fernando Colorado <108040168+LuisFerColorado@users.noreply.github.com>
2023-12-30 00:39:55 -0500

    ci(INIT): add src folder & some packages install

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>My App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
